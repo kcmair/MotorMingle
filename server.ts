9da6125ded6fa2ae70f2ed294aa149bed042213e
Keith Mair <mairck@gmail.com>
2023-03-31 19:43:37 -0600

    added session

--- server.ts
+++ server.ts
@@ -1,8 +1,10 @@
-import express from 'express'
+import express, { NextFunction } from 'express'
 import 'dotenv/config'
 const connectMongoose = require('./db/connect')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const session = require('express-session')
+const passport = require('passport')
 
 require('mongoose')
 
@@ -11,9 +13,12 @@ connectMongoose()
 const port = process.env.PORT || 3000
 const app = express()
 
+console.log(process.env.NODE_ENV)
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 if (process.env.NODE_ENV === 'development') {
+    console.log('I am here')
     const morgan = require('morgan')
     app.use(morgan('dev'))
 }
@@ -28,6 +33,19 @@ app.use(
     }
 )
 app.use(cors())
+app.use(
+    session({
+        secret: 'send it',
+        resave: false,
+        saveUninitialized: false,
+    })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(function (req: any, res: any, next: NextFunction) {
+    res.locals.user = req.user || null
+    next()
+})
 app.use('/', require('./routes'))
 app.listen(port, (): void => {
     console.log(`MotorMingle app listening on port ${port}`)
